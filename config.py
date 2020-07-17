3d555b8623665f5b702e8323dd5f07efece08b3d
Apple <apple@Apples-MacBook-Pro.local>
2020-07-17 17:00:27 +0530

    first commit

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python3
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
 import os
 
 class DefaultConfig:
-    """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("c756ac8a-50ea-4c78-99ee-4f1c00ab61c3")
+    APP_PASSWORD = os.environ.get("SA.OhO&$]m_.{EqWx[xU!EcC[lX%3t3")
